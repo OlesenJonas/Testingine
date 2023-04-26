e8afa0183defcef4ee11e178364b2d15a28ddd8a
OlesenJonas <45714731+OlesenJonas@users.noreply.github.com>
2023-04-26 22:35:06 +0200

    fixed ImGui callbacks getting overwritten

--- src/libraries/intern/Engine/Engine.cpp
+++ src/libraries/intern/Engine/Engine.cpp
@@ -1,4 +1,5 @@
 #include "Engine.hpp"
+#include "ImGui/imgui.h"
 #include <ImGui/imgui_impl_glfw.h>
 #include <ImGui/imgui_impl_vulkan.h>
 
@@ -6,13 +7,15 @@ Engine::Engine() : mainWindow(1200, 800, "Vulkan Test", {{GLFW_MAXIMIZED, GLFW_T
 {
     ptr = this;
 
+    // This needs to come first here, otherwise these callbacks will override
+    // ImGuis callbacks set-up in renderer.init()
+    inputManager.init();
+
     resourceManager.init();
     renderer.init();
 
     mainCamera =
         Camera{static_cast<float>(mainWindow.width) / static_cast<float>(mainWindow.height), 0.1f, 1000.0f};
-
-    inputManager.init();
 
     glfwSetTime(0.0);
     Engine::get()->getInputManager()->resetTime();
@@ -40,6 +43,7 @@ void Engine::update()
 
     ImGui_ImplVulkan_NewFrame();
     ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 
     inputManager.update();
     if(!ImGui::GetIO().WantCaptureMouse && !ImGui::GetIO().WantCaptureKeyboard)
@@ -48,7 +52,6 @@ void Engine::update()
     }
     // Not sure about the order of UI & engine code
 
-    ImGui::NewFrame();
     ImGui::ShowDemoWindow();
     ImGui::Render();
 
